dffd271ab106366d700e6973d82d96240fcdeaf6
Dehemi Patabendige <dp.dehemisuvipul@gmail.com>
2025-04-11 15:31:25 +0530

    navbar_fixed

--- layouts/header.php
+++ layouts/header.php
@@ -22,45 +22,43 @@ session_start();
 <body>
   <!--Navbar-->
   <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
-
     <div class="container">
-      <img class="logo" src="assets/imgs/logo.png">
+      <a class="navbar-brand" href="index.php">
+        <img class="logo" src="assets/imgs/logo.png" alt="Logo">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a class="nav-link" href="index.php">Home</a>
           </li>
-
           <li class="nav-item">
             <a class="nav-link" href="shop.php">Shop</a>
           </li>
-
           <li class="nav-item">
             <a class="nav-link" href="https://www.facebook.com/people/SolidComputers-Negombo/100057675990991/">Blog</a>
           </li>
-
           <li class="nav-item">
             <a class="nav-link" href="contact.php">Contact Us</a>
           </li>
-
-          <li class="nav-item">
-            <a href="cart.php"><i class="fa-solid fa-bag-shopping">
+          <li class="nav-item d-flex align-items-center">
+            <a href="cart.php" class="me-3">
+              <i class="fa-solid fa-bag-shopping">
                 <?php if (isset($_SESSION['quantity']) && $_SESSION['quantity'] != 0) { ?>
                   <span class="cart-quantity"> <?php echo $_SESSION['quantity']; ?> </span>
                 <?php } ?>
               </i>
             </a>
-            <a href="account.php"><i class="fa-solid fa-user"></i></a>
+            <a href="account.php">
+              <i class="fa-solid fa-user"></i>
+            </a>
           </li>
-
-
         </ul>
-
       </div>
     </div>
   </nav>
+</body>
+</html>
